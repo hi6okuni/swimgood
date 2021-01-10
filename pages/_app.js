a9a64e01bdc0c83ab355460ad5f93d3aa4a52ee2
Hirokuni <user@USERnoMacBook.lan>
2021-01-11 01:22:44 +0900

    change title

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
-import { ChakraProvider } from "@chakra-ui/react"
+import { ChakraProvider, extendTheme } from "@chakra-ui/react"
 import { AppWrapper } from '../lib/context/state'; // import based on where you put it
+import '../styles/global.css'
+
 
 export default function App({ Component, pageProps }) {
   return (
-    <ChakraProvider resetCSS>
+    <ChakraProvider resetCSS theme={extendTheme({
+      fonts: {
+        body: "Mukta"
+      }
+    })}>
       <AppWrapper>
         <Component {...pageProps} />
       </AppWrapper>
