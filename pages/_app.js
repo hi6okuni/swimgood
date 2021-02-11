77bef6b8201fbda98c0f8bc1a0281ff4214a6208
Hirokuni <user@USERnoMacBook.lan>
2021-02-11 17:14:46 +0900

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,27 @@
+import React, { Fragment } from 'react'
+import Router from 'next/router'
+import * as gtag from '../lib/gtag'
+
 import { ChakraProvider, extendTheme } from "@chakra-ui/react"
 import { AppWrapper } from '../lib/context/state'; // import based on where you put it
 import '../styles/global.scss'
 
+// Notice how we track pageview when route is changed
+Router.events.on('routeChangeComplete', (url) => gtag.pageview(url))
+
 
 export default function App({ Component, pageProps }) {
   return (
-    <ChakraProvider resetCSS theme={extendTheme({
-      fonts: {
-        body: "Poppins"
-      }
-    })}>
-      <AppWrapper>
-        <Component {...pageProps} />
-      </AppWrapper>
-    </ChakraProvider>
+    <Fragment>
+      <ChakraProvider resetCSS theme={extendTheme({
+        fonts: {
+          body: "Poppins"
+        }
+      })}>
+        <AppWrapper>
+          <Component {...pageProps} />
+        </AppWrapper>
+      </ChakraProvider>
+    </Fragment>
   )
 }
---
+++ pages/_document.js
@@ -0,0 +1,62 @@
+import { Fragment } from 'react'
+import Document, { Head, Main, NextScript } from 'next/document'
+import FavIcon from '../public/images/favicon.ico'
+
+import { GA_TRACKING_ID } from '../lib/gtag'
+
+export default class CustomDocument extends Document {
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    const initialProps = await Document.getInitialProps(ctx)
+
+    // Check if in production
+    const isProduction = process.env.NODE_ENV === 'production'
+
+    return {
+      ...initialProps,
+      isProduction,
+    }
+  }
+
+  render() {
+    const { isProduction } = this.props
+
+    return (
+      <html lang="en">
+        <Head>
+          <link rel="shortcut icon" type="image/x-icon" href={FavIcon} />
+
+          {/* We only want to add the scripts if in production */}
+          {isProduction && (
+            <Fragment>
+              {/* Global Site Tag (gtag.js) - Google Analytics */}
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+              />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', '${GA_TRACKING_ID}', {
+                      page_path: window.location.pathname,
+                    });
+                  `,
+                }}
+              />
+            </Fragment>
+          )}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
