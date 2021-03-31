7c5c30fcb5ed0c3faf6b706ccfce904e1810563b
Hirokuni <user@USERnoMacBook.lan>
2021-03-31 22:10:25 +0900

    fix djia page

--- pages/djia.js
+++ pages/djia.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import { useAppContext } from '../lib/context/state'
 import { Box, Flex, Center, Text} from "@chakra-ui/react"
-import AdsCard from '../../components/adscard'
+import AdsCard from '../components/adscard'
 import Link from 'next/link'
 
 export async function getStaticProps() {
