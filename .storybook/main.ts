b873aca797a55970870da425caba619cc0889540
hi6okuni <hi6okuni@gmail.com>
2023-04-09 19:19:18 +0900

    feat: add chakra addon of stroybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,17 +1,16 @@
-import type { StorybookConfig } from "@storybook/nextjs";
-const config: StorybookConfig = {
-  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+module.exports = {
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@chakra-ui/storybook-addon",
   ],
-  framework: {
-    name: "@storybook/nextjs",
-    options: {},
+  features: {
+    emotionAlias: false,
   },
-  docs: {
-    autodocs: "tag",
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-webpack5",
   },
 };
-export default config;
