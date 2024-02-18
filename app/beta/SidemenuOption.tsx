f43667221cf70b3181b75bb6825b7f07378f611c
hi6okuni <hi6okuni@gmail.com>
2024-02-18 20:36:06 +0900

    feat: sidemenu 2

--- app/beta/SidemenuOption.tsx
+++ app/beta/SidemenuOption.tsx
@@ -1,12 +1,18 @@
+import Link from 'next/link'
+
 type PropsType = {
 	value: string
 	isSelected: boolean
-	iconPath: JSX.Element
+	iconPath: {
+		outline: JSX.Element
+		solid: JSX.Element
+	}
 }
 
 export const SidemenuOption = ({ value, isSelected, iconPath }: PropsType) => {
-	return (
-		<li key={value} className={''}>
+	// searchはページ遷移せず、モーダル表示になる
+	return value === 'search' ? (
+		<li key={value} className={'cursor-pointer'}>
 			<svg
 				xmlns='http://www.w3.org/2000/svg'
 				fill={'none'}
@@ -16,8 +22,26 @@ export const SidemenuOption = ({ value, isSelected, iconPath }: PropsType) => {
 				role='img'
 				aria-label={value}
 			>
-				{iconPath}
+				{iconPath.outline}
 			</svg>
 		</li>
+	) : (
+		<Link href={`/beta/${value.toLowerCase()}`}>
+			<li key={value} className={''}>
+				<svg
+					xmlns='http://www.w3.org/2000/svg'
+					fill={'none'}
+					viewBox='0 0 24 24'
+					strokeWidth={1.5}
+					className={
+						isSelected ? 'w-6 h-6 fill-zinc-50' : 'w-6 h-6 stroke-zinc-50'
+					}
+					role='img'
+					aria-label={value}
+				>
+					{isSelected ? iconPath.solid : iconPath.outline}
+				</svg>
+			</li>
+		</Link>
 	)
 }
---
+++ app/beta/profile/page.tsx
@@ -0,0 +1,3 @@
+export default function ProfilePage() {
+	return <div className='text-zinc-900'>profile</div>
+}
